d5d21092efa2060a1465df093ba4d1f6badce0aa
thetimecabinets <thetimecabinets@gmail.com>
2025-05-24 00:37:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import json
 
 app = Flask(__name__)
 
-@app.route("/api/orders")
+@app.route('/api/orders')
 def get_orders():
-    with open("data.json", "r") as f:
+    with open('data.json') as f:
         data = json.load(f)
     return jsonify(data)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
